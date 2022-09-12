69768828c77ce2a5b3d4eead0e623c03bb64603b
kapil-panchal <kapil.panchal@nouvelletechnology.com.au>
2022-09-12 14:56:12 +0530

    BASE: APP Added empty test cases

---
+++ spring-reviews-collection/src/test/java/com/app60/springreviewscollection/api/DealerController_GETTest.java
@@ -0,0 +1,18 @@
+package com.app60.springreviewscollection.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DealerController_GETTest {
+
+    @Test
+    void getAllDealersWithLocationAndReviews() {
+    }
+
+    @Test
+    void getDealersByDealerName() {
+    }
+}
